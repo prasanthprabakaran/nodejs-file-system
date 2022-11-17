f03f3f74a8fa9ff53a382ab197450dbe44a3ab32
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-11-17 07:39:39 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import cors from 'cors';
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 
 app.use(cors());
